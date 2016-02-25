78f8836b8adc2a5e76c90b86f45cf9fcafa55708
Chris Roberts <code@chrisroberts.org>
2016-02-25 13:55:49 -0800

    Include deep linking to newly created configuration item

--- lib/fission-app-services/subscriptions/default_config.rb
+++ lib/fission-app-services/subscriptions/default_config.rb
@@ -1,18 +1,19 @@
 require 'fission-app'
 
 # Default configuration pack event hook
+# TODO: This is hard coded to request stack information. Should be plan dependent
 FissionApp.subscribe(/^before_(render|redirect)\..+?\.routes\.fission_app/) do |*args|
   event = ActiveSupport::Notifications::Event.new(*args)
   account = event.payload[:account]
   default_config = account.account_configs_dataset.where(:name => 'default').first
   unless(default_config)
-    Fission::Data::Models::AccountConfig.create(
+    default_config = Fission::Data::Models::AccountConfig.create(
       :name => 'default',
       :account_id => account.id
     )
     Fission::Data::Models::Notification.create(
       :subject => 'Default configuration created!',
-      :message => 'Go configure your services!'
+      :message => "A default configuration pack has been created for you. [Click here to get started!](#{Rails.application.routes.url_helpers.edit_config_path(default_config, :auto_add => [:stacks])})"
     ).add_account(account)
   end
 end
